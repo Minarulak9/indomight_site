4f48e8ac8eca62ab5785b7b2642006458c700cbd
minarul hoque <minarulak88@gmail.com>
2022-10-20 17:09:15 +0530

    header section done

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const notificationToggler = document.querySelector(".nav-toggler-btn");
+const navLinks = document.querySelector(".nav-links");
+const header = document.querySelector("header");
+const nav = document.querySelector(".nav");
+
+notificationToggler.addEventListener("click", () => {
+  navLinks.classList.contains("mob-active")
+    ? navLinks.classList.remove("mob-active")
+    : navLinks.classList.add("mob-active");
+});
+
+let options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.90,
+};
+const navObserver = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting && entry.intersectionRatio < 1) {
+        nav.classList.remove('scroll-active')
+        navLinks.classList.remove('scroll-active')
+    }else if(!entry.isIntersecting){
+        nav.classList.add('scroll-active')
+        navLinks.classList.add('scroll-active')
+    }
+    console.log(entry);
+  });
+}, options);
+navObserver.observe(header)
