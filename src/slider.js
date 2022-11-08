617b61de54e140052a717c1bb2be21e03db4d01b
minarul hoque <minarulak88@gmail.com>
2022-11-08 20:57:39 +0530

    slider done

--- src/slider.js
+++ src/slider.js
@@ -1,183 +1,36 @@
-function _getClosest(item, array, getDiff) {
-    var closest,
-        diff;
+const slider = document.querySelector(".slider");
+const sliders = document.querySelectorAll(".slide");
+let screen = window.innerWidth;
+let sliderWidth = sliders.length * 800;
+let initial = 500;
+slider.style.width = `calc(${sliderWidth}px + 2rem`;
 
-    if (!Array.isArray(array)) {
-        throw new Error("Get closest expects an array as second argument");
+let autoSlide;
+if (screen < 800) {
+  sliderWidth = sliders.length * 350;
+  initial = 200;
+}
+function autoSlider(px) {
+  autoSlide = setInterval(() => {
+    // console.log(slider.style.transform);
+    if (sliderWidth + initial < 0) {
+      initial = 500;
     }
-
-    array.forEach(function (comparedItem, comparedItemIndex) {
-        var thisDiff = getDiff(comparedItem, item);
-
-        if (thisDiff >= 0 && (typeof diff == "undefined" || thisDiff < diff)) {
-            diff = thisDiff;
-            closest = comparedItemIndex;
-        }
-    });
-
-    return closest;
+    slider.style.transform = `translateX(${(initial -= px)}px)`;
+  }, 0);
 }
-
-function number(item, array) {
-  return _getClosest(item, array, function (comparedItem, item) {
-    return Math.abs(comparedItem - item);
-  });
-}
-    
-function lerp(a, b, n) {
-    return (1 - n) * a + n * b
-}
-
-class Slider {
-  constructor(options = {}) {
-    this.bind()
-    
-    this.opts = {
-      el: options.el || '.js-slider',
-      ease: options.ease || 0.1,
-      speed: options.speed || 1.5,
-      velocity: 100,
-      scroll: options.scroll || false
-    }
-    
-    this.slider = document.querySelector('.js-slider')
-    this.sliderInner = this.slider.querySelector('.js-slider__inner')
-    this.slides = [...this.slider.querySelectorAll('.js-slide')]
-    this.slidesNumb = this.slides.length
-    
-    this.rAF = undefined
-    
-    this.sliderWidth = 0
-    
-    this.onX = 0
-    this.offX = 0
-    
-    this.currentX = 0
-    this.lastX = 0
-    
-    this.min = 0
-    this.max = 0
-
-    this.centerX = window.innerWidth / 2
-  }
-  
-  bind() {
-    ['setPos', 'run', 'on', 'off', 'resize'].forEach((fn) => this[fn] = this[fn].bind(this))
-  }
-  
-  setBounds() {
-    const bounds = this.slides[0].getBoundingClientRect()
-    const slideWidth = bounds.width ;
-
-    this.sliderWidth = (this.slidesNumb * slideWidth)*(50*this.slidesNumb)
-    this.max = -(this.sliderWidth - window.innerWidth)
-    
-    this.slides.forEach((slide, index) => {
-      slide.style.left = `${index * slideWidth}px`
-    })
-  }
-  
-  setPos(e) {
-    if (!this.isDragging) return
-    this.currentX = this.offX + ((e.clientX - this.onX) * this.opts.speed)
-    this.clamp()
-  }
-
-  clamp() {
-    this.currentX = Math.max(Math.min(this.currentX, this.min), this.max)
-  }
-  
-  run() {
-    this.lastX = lerp(this.lastX, this.currentX, this.opts.ease)
-    this.lastX = Math.floor(this.lastX * 100) / 100 
-
-    const sd = this.currentX - this.lastX
-    const acc = sd / window.innerWidth
-    let velo =+ acc
-    this.sliderInner.style.transform = `translate3d(${this.lastX}px, 0, 0) skewX(${velo * this.opts.velocity}deg)`
-    this.requestAnimationFrame()
-  }
-  
-  on(e) {
-    this.isDragging = true
-    this.onX = e.clientX
-    this.slider.classList.add('is-grabbing')
-  }
-  
-  off(e) {
-    this.snap()
-    this.isDragging = false
-    this.offX = this.currentX
-    this.slider.classList.remove('is-grabbing')
-  }
-  
-  closest() {
-    const numbers = []
-    this.slides.forEach((slide, index) => {
-      const bounds = slide.getBoundingClientRect()
-      const diff = this.currentX - this.lastX
-      const center = (bounds.x + diff) + (bounds.width / 2)
-      const fromCenter = this.centerX - center
-      numbers.push(fromCenter)
-    })
-
-    let closest = number(0, numbers)
-    closest = numbers[closest]
-    
-    return {
-      closest
-    }
-  }
-
-  snap() {
-    const { closest } = this.closest()
-    
-    this.currentX = this.currentX + closest
-    this.clamp()
-  }
-
-  requestAnimationFrame() {
-    this.rAF = requestAnimationFrame(this.run)
-  }
-
-  cancelAnimationFrame() {
-    cancelAnimationFrame(this.rAF)
-  }
-  
-  addEvents() {
-    this.run()
-    
-    this.slider.addEventListener('mousemove', this.setPos, { passive: true })
-    this.slider.addEventListener('mousedown', this.on, false)
-    this.slider.addEventListener('mouseup', this.off, false)
-    
-    window.addEventListener('resize', this.resize, false)
-  }
-  
-  removeEvents() {
-    this.cancelAnimationFrame(this.rAF)
-    
-    this.slider.removeEventListener('mousemove', this.setPos, { passive: true })
-    this.slider.removeEventListener('mousedown', this.on, false)
-    this.slider.removeEventListener('mouseup', this.off, false)
-  }
-  
-  resize() {
-    this.setBounds()
-  }
-  
-  destroy() {
-    this.removeEvents()
-    
-    this.opts = {}
-  }
-  
-  init() {
-    this.setBounds()
-    this.addEvents()
-  }
-}
-
-const slider = new Slider()
-slider.init()
-
+autoSlider(0.2);
+slider.addEventListener("mouseover", () => {
+  clearInterval(autoSlide);
+});
+slider.addEventListener("mouseout", () => {
+  clearInterval(autoSlide);
+  autoSlider(0.2);
+});
+slider.addEventListener("touchstart", () => {
+  clearInterval(autoSlide);
+});
+slider.addEventListener("touchend", () => {
+  clearInterval(autoSlide);
+  autoSlider(0.2);
+});
