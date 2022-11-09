d772e081fe2fdb43d6b32c36840d71bf69c062fd
minarul hoque <minarulak88@gmail.com>
2022-11-09 20:23:43 +0530

    all done

--- src/slider.js
+++ src/slider.js
@@ -1,25 +1,45 @@
 const slider = document.querySelector(".slider");
+const sliderContainer = document.querySelector("#slider");
 const sliders = document.querySelectorAll(".slide");
-let screen = window.innerWidth;
-let sliderWidth = sliders.length * 800;
-let initial = 500;
+let sliderWidth = sliders.length * (300 + 26);
+let initial = 50;
 slider.style.width = `calc(${sliderWidth}px + 2rem`;
 
 let autoSlide;
-if (screen < 800) {
-  sliderWidth = sliders.length * 350;
-  initial = 200;
-}
 function autoSlider(px) {
+  let sliderContainerWidth = sliderContainer.getBoundingClientRect().width;
   autoSlide = setInterval(() => {
     // console.log(slider.style.transform);
-    if (sliderWidth + initial < 0) {
-      initial = 500;
+    if (sliderWidth + initial < sliderContainerWidth ) {
+      clearInterval(autoSlide);
+      setTimeout(() => {
+        initial = 50;
+        autoSlider(0.2);
+      }, 3000);
+    } else {
+      slider.style.transform = `translateX(${(initial -= px)}px)`;
     }
-    slider.style.transform = `translateX(${(initial -= px)}px)`;
   }, 0);
 }
-autoSlider(0.2);
+
+let options1 = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.5,
+};
+const sliderObserver = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      setTimeout(() => {
+        clearInterval(autoSlide);
+        autoSlider(0.2);
+      }, 3000);
+      sliderObserver.disconnect();
+    }
+  });
+}, options1);
+sliderObserver.observe(slider);
+
 slider.addEventListener("mouseover", () => {
   clearInterval(autoSlide);
 });
